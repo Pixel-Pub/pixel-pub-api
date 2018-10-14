eb8fddcbb99065ca4a7bcb28f525ab2afbe3f3d5
Juan <abrecan.eyndrill@gmail.com>
2018-10-14 14:52:19 -0400

    update things

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,26 +1,35 @@
 import axios from 'axios'
-import btoa from 'btoa'
+import qs from 'qs'
 
 export default class AuthService {
+    static get auth() {
+        return {
+            username: process.env.CLIENT_ID,
+            password: process.env.CLIENT_SECRET
+        }
+    }
     static get headers() {
         return {
             'X-API-KEY'    : process.env.BUNGIE_API_KEY,
-            'Content-Type' : 'application/x-www-form-url-encoded',
-            'Authorization': 'Basic ' + btoa(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`)    
+            'Content-Type' : 'application/x-www-form-url-encoded',  
         }
     }
 
     static async refreshToken(refresh) {
         const url = 'https://www.bungie.net/platform/app/oauth/token/'
-        const {response} = await axios({
+        const config = {
             url,
             method  : 'POST',
+            auth    : AuthService.auth,
             headers : AuthService.headers,
-            data    : {
-                grant_type: refresh_token,
+            data    : qs.stringify({
+                grant_type: 'refresh_token',
                 refresh_token: refresh
-            }
-        })
+            })
+        }
+
+        config.params = config.data
+        const {response} = await axios(config)
 
         const {access_token, refresh_token, membership_id} = response.data
 
